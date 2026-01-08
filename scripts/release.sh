5be908621b34a076935b2f60d90b804b4f1d164b
Johannes Hof <hof_j@ukw.de>
2026-01-08 17:38:51 +0100

    setup release workflow

---
+++ scripts/release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -xe
+
+# Restore all git changes
+git restore --source=HEAD --staged --worktree -- package.json yarn.lock
+
+# Update token
+if [[ ! -z ${NPM_TOKEN} ]] ; then
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
+  echo "always-auth=true" >> ~/.npmrc
+  npm whoami
+fi
+
+# Release package
+echo "⚡ Publishing with tag latest"
+npx npm@8.17.0 publish --tag latest --access public --tolerate-republish
